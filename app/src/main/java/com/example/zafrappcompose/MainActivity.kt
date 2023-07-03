9b052f0e6d7a80bcc974d1a6c60b060383253ce1
YaelGonzalez08 <128552889+YaelGonzalez08@users.noreply.github.com>
2023-07-03 17:29:25 -0600

    agregar if

--- app/src/main/java/com/example/zafrappcompose/MainActivity.kt
+++ app/src/main/java/com/example/zafrappcompose/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fun comprarGalletas(){
+            val comprar=0
+
+
+        }
+
+
     }
 }
